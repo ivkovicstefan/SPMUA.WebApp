c06e1ce68037472ed645f1120f5539f70a9d8ae6
Stefan Ivković <ivkovics@outlook.com>
2023-09-17 21:48:26 +0200

    feat(admin): Implement functionality for admin login screen

--- src/views/admin/AdminLoginView.vue
+++ src/views/admin/AdminLoginView.vue
@@ -2,15 +2,32 @@
 import { ref } from 'vue'
 import InputText from 'primevue/inputtext';
 import Password from 'primevue/password'
+import Message from 'primevue/message';
 import Button from 'primevue/button'
+import { useAuthStore } from '@/stores/auth.store'
+import { useRouter } from 'vue-router'
+
+const authStore = useAuthStore()
+const router = useRouter()
 
 const email = ref('');
 const password = ref('');
+const errorMessage = ref('')
+
+const onLoginClick = async () => {
+    let response = await authStore.login(email.value, password.value)
+
+    if (response.isSuccessful) {
+        router.push('/admin/dashboard')
+    } else {
+        errorMessage.value = response.data
+    }
+}
 </script>
 
 <template>
     <div class="min-h-screen flex items-center justify-center bg-zinc-50">
-        <div class="flex flex-col p-6 w-full md:w-[400px] mt-[-50px]">
+        <div class="flex flex-col p-6 w-full md:w-[430px] mt-[-50px]">
             <img src="@/assets/logo-light.png" class="h-[120px] self-center mb-6"/>
             <h1 class="text-2xl text-center !text-black font-light mb-6">Uloguj se kao administrator</h1>
             <div class="flex flex-col gap-2 border rounded bg-white p-6 mb-6">
@@ -20,7 +37,9 @@ const password = ref('');
                 <label class="font-medium" for="email">Lozinka</label>
                 <Password v-model="password" :feedback="false" toggleMask class="mb-6"></Password>
 
-                <Button class="text-center">Uloguj se</Button>
+                <Message v-if="errorMessage.length > 0" severity="error" class="!mt-0 text-sm">{{ errorMessage }}</Message>
+
+                <Button class="text-center" :loading="authStore.isApiRequestLoading" @click="onLoginClick()">Uloguj se</Button>
             </div>
             <div class="flex flex-col gap-2 border rounded bg-white p-6">
                 <p class="text-sm text-medium text-justify">
@@ -39,5 +58,9 @@ const password = ref('');
 
 .p-button {
     @apply justify-center
+}
+
+.p-message-text {
+    @apply !text-sm
 }
 </style>
