03a2610563c95f15c513e353bbfba92ee04fd699
Stefan Ivković <ivkovics@outlook.com>
2023-11-12 14:39:22 +0100

    feat(home): Add reservation step wizard

--- src/views/public/HomeView.vue
+++ src/views/public/HomeView.vue
@@ -1,6 +1,41 @@
 <script setup lang="ts">
+import AppStepWizard from '@/components/AppStepWizard.vue'
+
+const stepWizardItems = [
+    {
+        stepTitle: 'Odabir usluge'
+    },
+    {
+        stepTitle: 'Odabir termina'
+    },
+    {
+        stepTitle: 'Kontakt podaci'
+    },
+    {
+        stepTitle: 'Pregled rezervacije'
+    }
+]
 </script>
 
 <template>
-    Home
+    <!-- Hero Section -->
+    <!-- Gallery Section -->
+    <!-- Book Appointment Section-->
+    <div>
+        <AppStepWizard
+            :items="stepWizardItems">
+            <template #step1Content>
+                Step 1
+            </template>
+            <template #step2Content>
+                Step 2
+            </template>
+            <template #step3Content>
+                Step 3
+            </template>
+            <template #step4Content>
+                Step 4
+            </template>
+        </AppStepWizard>
+    </div>
 </template>
