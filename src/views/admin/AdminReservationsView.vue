8a4d0498cff3328a956ff6555525a34392a58510
Stefan Ivković <ivkovics@outlook.com>
2024-01-28 14:22:29 +0100

    feat(admin-reservations): Confirmed reservations section

--- src/views/admin/AdminReservationsView.vue
+++ src/views/admin/AdminReservationsView.vue
@@ -22,9 +22,14 @@ const appointmentStore = useAppointmentStore()
 const { appointments, patchAppointmentStatus } = appointmentStore
 
 const pendingAppointmentsExpandedRows = ref([])
+const confirmedAppointmentsExpandedRows = ref([])
 
 const computedPendingAppointments = computed(() => {
   return appointments.data.filter((a) => a.appointmentStatusId == 1)
+})
+
+const computedConfirmedAppointments = computed(() => {
+  return appointments.data.filter((a) => a.appointmentStatusId == 2)
 })
 
 appointmentStore.getAppointments()
@@ -72,9 +77,9 @@ const onReservationResponseConfirm = async (): Promise<void> => {
 <template>
   <h1 class="text-2xl text-black font-light mb-6">Rezervacije</h1>
 
-  <div class="grid grid-cols-4">
+  <div class="grid grid-cols-4 gap-6">
     <div class="col-span-4 lg:col-span-3">
-      <Panel toggleable>
+      <Panel>
         <template #header>
           <div class="flex">
             <h1 class="font-semibold">Zahtevi za rezervaciju</h1>
@@ -208,6 +213,132 @@ const onReservationResponseConfirm = async (): Promise<void> => {
           </Dialog>
           <InlineMessage
             v-if="computedPendingAppointments.length == 0"
+            class="!m-6"
+            severity="info"
+          >
+            Trenutno nema rezervacija na čekanju.
+          </InlineMessage>
+        </div>
+        <div class="flex items-center min-h-[300px]" v-if="appointments.isLoading">
+          <ProgressSpinner
+            style="height: 50px; width: 50px"
+            strokeWidth="4"
+            animationDuration=".5s"
+          />
+        </div>
+      </Panel>
+    </div>
+    <div class="col-span-4 lg:col-span-2">
+      <Panel>
+        <template #header>
+          <div class="flex">
+            <h1 class="font-semibold">Potvrđene rezervacije</h1>
+            <Badge
+              v-if="appointments.isFinished"
+              :value="computedConfirmedAppointments.length"
+              class="ml-1"
+            ></Badge>
+          </div>
+        </template>
+        <div class="flex flex-col" v-if="appointments.isFinished">
+          <DataTable
+            v-if="computedConfirmedAppointments.length > 0"
+            :value="computedConfirmedAppointments"
+            class="p-datatable-sm headerless"
+            v-model:expanded-rows="confirmedAppointmentsExpandedRows"
+            dataKey="appointmentId"
+            striped-rows
+            paginator
+            :rows="5"
+          >
+            <Column expander />
+            <template #expansion="slotProps">
+              <div class="p-3">
+                <table>
+                  <tr>
+                    <th>Broj rezervacije:</th>
+                    <td class="pl-3">{{ slotProps.data.appointmentId }}</td>
+                  </tr>
+                  <tr>
+                    <th>Datum:</th>
+                    <td class="pl-3">
+                      {{ useDefaultDateFormatter(slotProps.data.appointmentDate) }}
+                    </td>
+                  </tr>
+                  <tr>
+                    <th>Vreme:</th>
+                    <td class="pl-3">
+                      {{ useDefaultTimeFormatter(slotProps.data.appointmentDate) }}
+                    </td>
+                  </tr>
+                  <tr>
+                    <th>Email adresa:</th>
+                    <td class="pl-3">{{ slotProps.data.customerEmail }}</td>
+                  </tr>
+                  <tr>
+                    <th>Broj telefona:</th>
+                    <td class="pl-3">
+                      <a :href="'tel:' + slotProps.data.customerPhone">{{
+                        slotProps.data.customerPhone
+                      }}</a>
+                    </td>
+                  </tr>
+                </table>
+              </div>
+            </template>
+            <Column>
+              <template #body="slotProps">
+                {{ slotProps.data.customerFirstName + ' ' + slotProps.data.customerLastName }}
+              </template>
+            </Column>
+            <Column field="serviceTypeName" header="Usluga"></Column>
+            <Column>
+              <template #body="slotProps">
+                <div class="flex items-center">
+                </div>
+              </template>
+            </Column>
+          </DataTable>
+          <Dialog
+            v-model:visible="isConfirmOrRejectAppointmentModalVisible"
+            modal
+            :header="confirmOrRejectAppointmentModalHeader"
+          >
+            <div class="flex flex-col">
+              <label for="txtComment" class="font-medium mb-2">Komentar</label>
+              <Textarea
+                v-model.value="reservationResponseObject.responseComment"
+                id="txtComment"
+                rows="5"
+                cols="30"
+                v-tooltip="
+                  'Komentar će biti poslat klijentu u okviru automatskog email-a nakon odobravanja ili odbijanja rezervacije.'
+                "
+              ></Textarea>
+            </div>
+            <template #footer>
+              <Button
+                v-if="confirmOrRejectAppointmentModalMode == AppointmentStatusChangeAction.Reject"
+                severity="danger"
+                class="!w-full"
+                label="Potvrdi"
+                @click="onReservationResponseConfirm"
+                :loading="patchAppointmentStatus.isLoading"
+              >
+              </Button>
+              <Button
+                v-if="confirmOrRejectAppointmentModalMode == AppointmentStatusChangeAction.Approve"
+                severity="success"
+                class="!w-full"
+                label="Potvrdi"
+                @click="onReservationResponseConfirm"
+                :loading="patchAppointmentStatus.isLoading"
+              >
+              </Button>
+            </template>
+          </Dialog>
+          <InlineMessage
+            v-if="computedConfirmedAppointments.length == 0"
             class="!m-6"
             severity="info"
           >
