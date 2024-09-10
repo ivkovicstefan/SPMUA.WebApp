10c6f123e78f7787fe5402762478986c7fc073bc
Stefan Ivković <ivkovics@outlook.com>
2024-09-10 19:23:24 +0200

    feat(about-me): Improve content and layout

--- src/views/public/AboutMeView.vue
+++ src/views/public/AboutMeView.vue
@@ -1,21 +1,68 @@
 <template>
     <div class="flex flex-col">
-        <div class="px-10 py-5 lg:px-48">
-            <h1 class="leading-[1] text-[2.35rem] lg:text-5xl mb-8 lg:mb-12">O meni</h1>
-            <p class="text-zinc-500 text-lg text-justify">
-                Zdravo, ja sam Petra! Imam dvadeset dve godine i profesionalni sam šminker i edukator. Trenutno radim u Beogradu i Jagodini.
-            </p>
-            <p class="mt-3 text-zinc-500 text-lg text-justify">
-                Moja ljubav prema šminkanju počela je kada sam imala samo 16 godina. Od prvog trenutka kada sam uzela četkicu u ruke, znala sam da je to moj životni poziv. Ništa drugo nije budilo takvu strast i emociju u meni.
-            </p>
-            <p class="mt-3 text-zinc-500 text-lg text-justify">
-                Za mene šminkanje nije samo posao. To je moj način da pronađem mir i izrazim svoju kreativnost.
-                <br>
-                Najlepše je raditi ono što najviše voliš, iz razloga što te to podstiče da budeš bolji i od samog sebe. Kada nešto radiš sa ljubavlju, nijedan radni dan nije težak.
-            </p>
-            <p class="mt-3 text-zinc-500 text-lg text-justify flex gap-2 items-center">
-                Idemo zajedno kroz moj MIR
-                <svg fill="#cccccc" height="32px" width="32px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="-56.6 -56.6 584.90 584.90" xml:space="preserve" stroke="#cccccc" stroke-width="28.30206"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <path d="M433.601,67.001c-24.7-24.7-57.4-38.2-92.3-38.2s-67.7,13.6-92.4,38.3l-12.9,12.9l-13.1-13.1 c-24.7-24.7-57.6-38.4-92.5-38.4c-34.8,0-67.6,13.6-92.2,38.2c-24.7,24.7-38.3,57.5-38.2,92.4c0,34.9,13.7,67.6,38.4,92.3 l187.8,187.8c2.6,2.6,6.1,4,9.5,4c3.4,0,6.9-1.3,9.5-3.9l188.2-187.5c24.7-24.7,38.3-57.5,38.3-92.4 C471.801,124.501,458.301,91.701,433.601,67.001z M414.401,232.701l-178.7,178l-178.3-178.3c-19.6-19.6-30.4-45.6-30.4-73.3 s10.7-53.7,30.3-73.2c19.5-19.5,45.5-30.3,73.1-30.3c27.7,0,53.8,10.8,73.4,30.4l22.6,22.6c5.3,5.3,13.8,5.3,19.1,0l22.4-22.4 c19.6-19.6,45.7-30.4,73.3-30.4c27.6,0,53.6,10.8,73.2,30.3c19.6,19.6,30.3,45.6,30.3,73.3 C444.801,187.101,434.001,213.101,414.401,232.701z"></path> </g> </g></svg>            </p>
+        <div class="grid grid-cols-2 gap-20 px-10 py-6 pb-12 lg:px-48">
+            <div class="flex flex-col gap-y-3 col-span-2 lg:col-span-1 text-zinc-500">
+                <h1 class="text-black leading-[1] text-[2.35rem] lg:text-5xl mb-8 lg:mb-12">Upoznaj me</h1>
+                <p class="text-lg text-justify">
+                    Zdravo, ja sam Petra! Imam dvadeset dve godine i <b>profesionalni sam šminker i edukator</b>. Trenutno radim u <b>Beogradu i Jagodini</b>.
+                </p>
+                <p class="text-lg text-justify">
+                    Moja ljubav prema šminkanju počela je kada sam imala samo 16 godina. Od prvog trenutka kada sam uzela četkicu u ruke, znala sam da je to moj životni poziv. Ništa drugo nije budilo takvu strast i emociju u meni.
+                </p>
+                <p class="text-lg text-justify">
+                    Za mene šminkanje nije samo posao. To je moj način da pronađem mir i izrazim svoju kreativnost.
+                    <br>
+                    Najlepše je raditi ono što najviše voliš, iz razloga što te to podstiče da budeš bolji i od samog sebe. Kada nešto radiš sa ljubavlju, nijedan radni dan nije težak.
+                </p>
+            </div>
+            <div class="hidden lg:block">
+                <img class="object-cover rounded-xl h-[500px] w-full" src="@/assets/gallery-picture-5.jpg">
+            </div>
+        </div>
+        <div class="flex flex-col py-6 bg-zinc-50 px-10 py-11 lg:px-48 border-y">
+            <h1 class="text-black leading-[1] text-[2.35rem] lg:text-5xl mb-8 lg:mb-12">Šta kažu klijenti</h1>
+            <div class="grid grid-cols-1 lg:grid-cols-3 gap-6">
+                <div
+                    class="row-span-2 flex flex-col border border-zinc-300 rounded-xl bg-white p-3 gap-y-3">
+                    <div class="flex flex-row gap-3 items-center">
+                        <img class="rounded-[50%] object-cover bg-pink-200 h-[2rem] w-[2rem]" src="@/assets/gallery-picture-1.png">
+                        <div class="font-semibold text-black">Osoba 1</div>
+                    </div>
+                    <p class="text-left italic text-zinc-500">“ Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae error beatae nisi a ullam consequatur nesciunt deserunt nostrum illo, harum alias, doloribus magnam ratione accusamus? In minus vitae iusto distinctio? ”</p>
+                </div>
+                <div
+                    class="flex flex-col border border-zinc-300 rounded-xl bg-white p-3 gap-y-3">
+                    <div class="flex flex-row gap-3 items-center">
+                        <img class="rounded-[50%] object-cover bg-pink-200 h-[2rem] w-[2rem]" src="@/assets/gallery-picture-2.jpg">
+                        <div class="font-semibold text-black">Osoba 2</div>
+                    </div>
+                    <p class="text-left italic text-zinc-500">“ Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae error beatae nisi a ullam consequatur nesciunt deserunt nostrum illo, harum alias, doloribus magnam ratione accusamus? In minus vitae iusto distinctio? ”</p>
+                </div>
+                <div
+                    class="flex flex-col border border-zinc-300 rounded-xl bg-white p-3 gap-y-3">
+                    <div class="flex flex-row gap-3 items-center">
+                        <img class="rounded-[50%] object-cover bg-pink-200 h-[2rem] w-[2rem]" src="@/assets/gallery-picture-4.jpg">
+                        <div class="font-semibold text-black">Osoba 3</div>
+                    </div>
+                    <p class="text-left italic text-zinc-500">“ Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae error beatae nisi a ullam consequatur nesciunt deserunt nostrum illo, harum alias, doloribus magnam ratione accusamus? In minus vitae iusto distinctio? ”</p>
+                </div>
+                <div
+                    class="flex flex-col border border-zinc-300 rounded-xl bg-white p-3 gap-y-3">
+                    <div class="flex flex-row gap-3 items-center">
+                        <img class="rounded-[50%] object-cover bg-pink-200 h-[2rem] w-[2rem]" src="@/assets/gallery-picture-5.jpg">
+                        <div class="font-semibold text-black">Osoba 4</div>
+                    </div>
+                    <p class="text-left italic text-zinc-500">“ Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae error beatae nisi a ullam consequatur nesciunt deserunt nostrum illo, harum alias, doloribus magnam ratione accusamus? In minus vitae iusto distinctio? ”</p>
+                </div>
+                <div
+                    class="flex flex-col border border-zinc-300 rounded-xl bg-white p-3 gap-y-3">
+                    <div class="flex flex-row gap-3 items-center">
+                        <img class="rounded-[50%] object-cover bg-pink-200 h-[2rem] w-[2rem]" src="@/assets/gallery-picture-6.jpg">
+                        <div class="font-semibold text-black">Osoba 5</div>
+                    </div>
+                    <p class="text-left italic text-zinc-500">“ Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae error beatae nisi a ullam consequatur nesciunt deserunt nostrum illo, harum alias, doloribus magnam ratione accusamus? In minus vitae iusto distinctio? ”</p>
+                </div>
+            </div>
         </div>
     </div>
 </template>
